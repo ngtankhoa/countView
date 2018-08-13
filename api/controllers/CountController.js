e1ddfabcc175e1d45ba1787011d89e8af095c1cb
ngtankhoa <khoant.uit@gmail.com>
2018-08-13 18:02:56 +0700

    add full file

---
+++ api/controllers/CountController.js
@@ -0,0 +1,23 @@
+/**
+ * CountController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+
+    count_onload: async function (req,res) {
+        var numrecord = await Count.count({ countViewTime: 0 });
+        return res.view('pages/count', { num: numrecord });
+    },
+
+    count: async function (req, res) {
+        let countS = await Count.create();
+        this.count_onload;
+        numrecord = await Count.count({ countViewTime: 0 });
+        return res.view('pages/count', { num: numrecord });
+    }
+
+};
+
